254b04b0e81b26afb458644083e4a396787b8057
DoHKapJleoHe <91648774+DoHKapJleoHe@users.noreply.github.com>
2023-11-25 12:10:07 +0700

    initial commit

---
+++ src/main/java/com/changellenge/hackaton/sber/CompositApplication.java
@@ -0,0 +1,13 @@
+package com.changellenge.hackaton.sber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompositApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/changellenge/hackaton/sber/CompositApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changellenge.hackaton.sber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
